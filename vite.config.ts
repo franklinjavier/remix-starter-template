04b2d837a345197a37ecd84a0ab1a968d406a27d
Frank <franklinjalves@gmail.com>
2024-03-07 15:04:31 +0000

    chore: vercel remix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev'
+import { vercelPreset } from '@vercel/remix/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -6,6 +7,7 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ['**/.*'],
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],

de84d617546346fc1b0fccdb8aed0f8455ab34f8
Frank <franklinjalves@gmail.com>
2024-03-08 14:09:10 +0000

    fix: Server-only module referenced by client

---
+++ app/modules/auth/auth.server.ts
@@ -0,0 +1,3 @@
+export async function auth() {
+  return null
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,19 @@
 import { type MetaFunction } from '@remix-run/node'
 import { Title } from '~/components/title'
+import { auth, useUser } from '~/modules/auth'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Remix Starter Template' }, { name: 'description', content: 'Remix Starter Template' }]
 }
 
+export async function loader() {
+  await auth()
+  return null
+}
+
 export default function Index() {
+  const user = useUser()
+
   return (
     <main>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
@@ -14,6 +22,7 @@ export default function Index() {
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit
             sunt amet fugiat veniam occaecat fugiat aliqua.
+            {user.name}
           </p>
         </div>
       </div>

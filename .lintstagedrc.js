a03991b08c11f8761605ad47fe0080bcd9e799a5
Frank <franklinjalves@gmail.com>
2024-04-24 15:50:00 +0100

    chore: lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,24 +2,23 @@ import { quote as escape } from 'shell-quote'
 import { ESLint } from 'eslint'
 
 const eslint = new ESLint()
-const isWin = process.platform === 'win32';
+const isWin = process.platform === 'win32'
 
 const escapeFileNamesForPrettier = (filenames) =>
   filenames
     .map((filename) => `"${isWin ? filename : escape([filename])}"`)
-    .join(' ');
+    .join(' ')
 
 export default {
   '**/*.{js,jsx,ts,tsx}': (filenames) => {
-    console.log(filenames);
     return [
       `pnpm lint --fix ${filenames
         .filter((file) => !eslint.isPathIgnored(file))
         .map((f) => `"${f}"`)
         .join(' ')}`,
-    ];
+    ]
   },
   '**/*.{json,md,mdx,css,html,yml,yaml,scss}': (filenames) => {
-    return [`prettier --write ${escapeFileNamesForPrettier(filenames)}`];
+    return [`prettier --write ${escapeFileNamesForPrettier(filenames)}`]
   },
-};
+}

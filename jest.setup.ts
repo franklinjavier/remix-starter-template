1fdc6a12972cf24df74e964603aea29f8699cca2
Frank <franklinjalves@gmail.com>
2024-04-24 15:57:55 +0100

    chore: fix typecheck

--- jest.setup.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom'
-
-jest.mock('@remix-run/react', () => ({
-  ...jest.requireActual('@remix-run/react'),
-  useMatches: () => [{ pathname: '/', data: { user: { displayName: 'foo' } } }],
-}))
